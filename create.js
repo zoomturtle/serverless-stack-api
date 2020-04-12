08d1d336ac89d8568841f4826f271e1dcbc25576
zoomturtle <ddejesus@outlook.com>
2020-04-12 16:33:51 -0400

    Edited create.js

--- create.js
+++ create.js
@@ -27,6 +27,24 @@ export function main(event, context, callback) {
         }
     };
 
+    dynamoDb.put(params, (error, data) => {
+        // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+
+        // Return status code 500 on error
+        if (error) {
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false })
+            };
+            callback(null, response);
+            return;
+        }
+    }
     
 }
 
