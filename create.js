f9e1b5e0c78d1b7d940b9f3be630a232743673bb
zoomturtle <ddejesus@outlook.com>
2020-04-12 11:04:15 -0400

    Edited create.js script

--- create.js
+++ create.js
@@ -4,6 +4,29 @@ import AWS from "aws-sdk";
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.tablename,
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //              Cognito Identity Pool, we will use the identity id
+        //              as the user id of the authenticated user
+        // - 'noteId': a unique uuid
+        // - 'content': parsed from request body
+        // - 'attachment': parsed from request body
+        // - 'createdAt': current Unix timestamp
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+
+        }
+    };
+
     
 }
 
